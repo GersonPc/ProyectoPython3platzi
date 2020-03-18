2f8758c55f7d0d5d9f3bfa49e43d02e2245b6d95
GersonPc <gerpxd@gmail.com>
2020-03-17 22:15:57 -0600

    Implementando funcion de Listar clientes y actualizarlos

--- platzi-ventas/clients/services.py
+++ platzi-ventas/clients/services.py
@@ -1,5 +1,6 @@
 import csv
 from clients.models import Client
+import os
 
 
 class ClientService:
@@ -10,3 +11,28 @@ class ClientService:
         with open (self.table_name, mode='a') as f:
             writer = csv.DictWriter(f, fieldnames=Client.schema())
             writer.writerow(client.to_dict())
+
+    def list_clients(self):
+        with open (self.table_name, mode='r') as f:
+            reader = csv.DictReader(f, fieldnames=Client.schema())
+
+            return list(reader)
+    
+    def update_client(self, update_client):
+        clients = self.list_clients()
+        update_clients = []
+        for client in clients:
+            if client['uid'] == update_client.uid:
+                update_clients.append(update_client.to_dict())
+            else:
+                update_clients.append(client)
+        self._save_to_disk(update_clients)
+    
+    def _save_to_disk(clients):
+        tmp_table_name = self.table_name + '.tmp'
+        with open(tmp_table_name) as f:
+            writer = csv.DictWriter(f, fieldnames=Client.schema())
+            writer.writerows(clients)
+        
+        os.remove(self.table_name)
+        os.rename(tmp_table_name, self.table_name)
